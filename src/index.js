18904f02862286debd6c07a59bfdd66df063087d
Maggie Hedrick <maggie.hedrick@genesys.com>
2018-06-22 23:16:41 -0400

    rudimentary saving + reorg

--- src/index.js
+++ src/index.js
@@ -2,30 +2,33 @@ import React, {Fragment} from "react";
 import ReactDOM from "react-dom";
 
 class App extends React.Component {
-  state = {
-    elapsedTime: null,
+  initialState = {
+    seconds: 0,
     seconders: 0
   };
+  state = {};
+  componentWillMount() {
+    let savegame = JSON.parse(localStorage.getItem("save"));
+    if (savegame && typeof savegame.seconds != "undefined") {
+      this.setState(savegame);
+    } else {
+      this.setState(this.initialState);
+    }
+  }
+  componentDidMount(){
+    //start global tick
+    this.tick();
+  }
   seconderCost() {
     return Math.floor(10 * Math.pow(1.1, this.state.seconders));
   }
-  render() {
-    return (
-    <Fragment>
-      <button onClick={this.addSeconds.bind(this, 1)}>add 1 second</button>
-      <br />
-      <button onClick={this.buySeconder.bind(this, 1)} disabled={this.seconderCost() > this.state.elapsedTime}>buy 1 seconder for {this.seconderCost()} seconds</button>
-      <div>you have: {this.state.elapsedTime} seconds</div>
-      <div>you have: {this.state.seconders} seconders</div>
-    </Fragment>);
-  }
   buySeconder(){
-    if(this.seconderCost() <= this.state.elapsedTime){
-      this.setState({seconders: this.state.seconders + 1, elapsedTime: this.state.elapsedTime - this.seconderCost()});
+    if(this.seconderCost() <= this.state.seconds){
+      this.setState({seconders: this.state.seconders + 1, seconds: this.state.seconds - this.seconderCost()});
     }
   }
   addSeconds(seconds = 1){
-    this.setState({ elapsedTime: this.state.elapsedTime + seconds })
+    this.setState({ seconds: this.state.seconds + seconds })
   }
   tick(){
     // time until next second
@@ -35,9 +38,28 @@ class App extends React.Component {
 
     this.addSeconds(this.state.seconders || 1);
   }
-  componentDidMount(){
-    //start global tick
-    this.tick();
+  save() {
+    // todo: polyfill this once everythings in a set STATE (get it)
+    localStorage.setItem('save', JSON.stringify(this.state));
+  }
+  unsave() {
+    // todo: polyfill this once everythings in a set STATE (get it)
+    localStorage.removeItem('save', JSON.stringify(this.state));
+    this.setState(this.initialState);
+  }
+  render() {
+    return (
+    <Fragment>
+      <button onClick={this.addSeconds.bind(this, 1)}>add 1 second</button>
+      <br />
+      <button onClick={this.buySeconder.bind(this, 1)} disabled={this.seconderCost() > this.state.seconds}>buy 1 seconder for {this.seconderCost()} seconds</button>
+      <br />
+      <button onClick={this.save.bind(this)}>save game</button>
+      <br />
+      <button onClick={this.unsave.bind(this)}>delete save</button>
+      <div>you have: {this.state.seconds} seconds</div>
+      <div>you have: {this.state.seconders} seconders</div>
+    </Fragment>);
   }
 }
 

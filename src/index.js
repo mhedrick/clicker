b9720de764641f30de125b6fe62240db0c0f579d
Maggie Hedrick <maggie.hedrick@genesys.com>
2018-06-23 16:43:52 -0400

    update terms, get the timer workin a bit better

--- src/index.js
+++ src/index.js
@@ -3,40 +3,46 @@ import ReactDOM from "react-dom";
 
 class App extends React.Component {
   initialState = {
-    seconds: 0,
-    seconders: 0
+    power: 0,
+    readers: 0
   };
   state = {};
   componentWillMount() {
     let savegame = JSON.parse(localStorage.getItem("save"));
-    if (savegame && typeof savegame.seconds != "undefined") {
+    if (savegame && typeof savegame.power != "undefined") {
       this.setState(savegame);
     } else {
-      this.setState(this.initialState);
+      this.setState({ lastUpdate: Date.now(), ...this.initialState});
     }
   }
   componentDidMount(){
     //start global tick
     this.tick();
   }
-  seconderCost() {
-    return Math.floor(10 * Math.pow(1.1, this.state.seconders));
+  readerCost() {
+    return Math.floor(10 * Math.pow(1.35, this.state.readers));
   }
-  buySeconder(){
-    if(this.seconderCost() <= this.state.seconds){
-      this.setState({seconders: this.state.seconders + 1, seconds: this.state.seconds - this.seconderCost()});
+  buypower(){
+    if(this.readerCost() <= this.state.power){
+      this.setState({readers: this.state.readers + 1, power: this.state.power - this.readerCost()});
     }
   }
-  addSeconds(seconds = 1){
-    this.setState({ seconds: this.state.seconds + seconds })
+  addpower(power = 1){
+    this.setState({ power: this.state.power + power })
   }
   tick(){
-    // time until next second
-    // won't drift with time
-    const nextSecond = 1000 - Date.now() % 1000
-    this.timer = setTimeout(this.tick.bind(this), nextSecond)
+    let thisUpdate = Date.now();
+    // find the seconds since the last update
+    let delta = Math.round((thisUpdate - this.state.lastUpdate) / 1000);
+    // force updates to only happen in second intervals, nothing faster
+    if (delta < 1){
+        delta = 1;
+    }
+    const nextSecond = 1000 - thisUpdate % 1000
+    this.timer = setTimeout(this.tick.bind(this), nextSecond * delta);
+    this.addpower((this.state.readers * .1 + 1) * delta);
 
-    this.addSeconds(this.state.seconders || 1);
+    this.setState({ lastUpdate: Date.now()});
   }
   save() {
     // todo: polyfill this once everythings in a set STATE (get it)
@@ -45,20 +51,21 @@ class App extends React.Component {
   unsave() {
     // todo: polyfill this once everythings in a set STATE (get it)
     localStorage.removeItem('save', JSON.stringify(this.state));
-    this.setState(this.initialState);
+    this.setState({ lastUpdate: Date.now(), ...this.initialState });
   }
   render() {
     return (
     <Fragment>
-      <button onClick={this.addSeconds.bind(this, 1)}>add 1 second</button>
+      <button onClick={this.addpower.bind(this, 1)}>add 1 power</button>
       <br />
-      <button onClick={this.buySeconder.bind(this, 1)} disabled={this.seconderCost() > this.state.seconds}>buy 1 seconder for {this.seconderCost()} seconds</button>
+      <button onClick={this.buypower.bind(this, 1)} disabled={this.readerCost() > this.state.power}>split 1 reader for {this.readerCost()} power</button>
       <br />
       <button onClick={this.save.bind(this)}>save game</button>
       <br />
       <button onClick={this.unsave.bind(this)}>delete save</button>
-      <div>you have: {this.state.seconds} seconds</div>
-      <div>you have: {this.state.seconders} seconders</div>
+      {/* on real display, prettify */}
+      <div>you have: {this.state.power} power</div>
+      <div>you have: {this.state.readers} readers</div>
     </Fragment>);
   }
 }
